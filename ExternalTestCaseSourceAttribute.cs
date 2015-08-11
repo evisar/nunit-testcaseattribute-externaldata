087f92e1c881ab0d883ea4df0024d7e7de3f8489
evisar <evisar@gmail.com>
2015-08-11 13:53:57 +0200

    ExternalTestCaseSourceAttribute for NUnit.
    Setup e TestData folder, and you can specify test cases in JSON, or any
    other format using the ITestCaseSource interface.

---
+++ ExternalTestCaseSourceAttribute.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tsa.automation.sts2015.common
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class ExternalTestCaseSourceAttribute : TestCaseSourceAttribute
+    {
+        [SetUpFixture]
+        private class DataTestCase : IDisposable
+        {
+            static object _sync = new object();
+
+            [ThreadStatic]
+            internal static string[] Args;
+            [ThreadStatic]
+            internal static ITestCaseSource DataSource;
+
+            public IEnumerable TestCases
+            {
+                get
+                {
+                    lock (_sync)
+                    {
+                        return DataSource.GetTestCases(Args);
+                    }
+                }
+
+            }
+
+            [TearDown]
+            public void Release()
+            {
+                DataSource = null;
+                Args = null;
+            }
+
+            public void Dispose()
+            {
+                Release();
+            }
+        }
+
+        public ExternalTestCaseSourceAttribute(Type dataSourceType, [CallerFilePath] string classPath = null, [CallerMemberName] string methodName = null)
+            : base(typeof(DataTestCase), "TestCases")
+        {
+            DataTestCase.DataSource = Activator.CreateInstance(dataSourceType) as ITestCaseSource;
+            DataTestCase.Args = new string[] { classPath, methodName };
+        }
+
+        public ExternalTestCaseSourceAttribute([CallerFilePath] string classPath = null, [CallerMemberName] string methodName = null)
+            : base(typeof(DataTestCase), "TestCases")
+        {
+            Type dataSourceType = Type.GetType(ConfigurationManager.AppSettings["TestDataSourceType"]);
+            DataTestCase.DataSource = Activator.CreateInstance(dataSourceType) as ITestCaseSource;
+            DataTestCase.Args = new string[] { classPath, methodName }; 
+        }
+    }
+}
---
+++ JsonTestCaseSource.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tsa.automation.sts2015.common;
+using tsa.automation.sts2015.WCF;
+
+namespace NunitTestCaseSourceExternal
+{
+    public class JsonTestCaseSource : ITestCaseSource
+    {
+        public System.Collections.IEnumerable GetTestCases(params object[] args)
+        {
+            string classPath = args.First().ToString();
+            string methodName = args.Last().ToString();
+
+            var testFolder = Path.GetFileNameWithoutExtension(classPath);
+
+            string parentFilePath = string.Format("TestData\\{0}\\template.json", ConfigurationManager.AppSettings["Environment"]);
+            JObject parentTemplate = null;
+            if (File.Exists(parentFilePath))
+            {
+                var json = File.ReadAllText(parentFilePath);
+                parentTemplate = (JObject)JsonConvert.DeserializeObject(json);
+            }
+
+            string baseFilePath = string.Format("TestData\\{0}\\{1}\\template.json", ConfigurationManager.AppSettings["Environment"], testFolder);
+            JObject template = parentTemplate;
+            if (File.Exists(baseFilePath))
+            {
+                var json = File.ReadAllText(baseFilePath);
+                template = (JObject)JsonConvert.DeserializeObject(json);
+                template.Merge(parentTemplate);
+            }
+            
+            string filePath = string.Format("TestData\\{0}\\{1}\\{2}.json", ConfigurationManager.AppSettings["Environment"], testFolder, methodName);
+            if (File.Exists(filePath))
+            {
+                var json = File.ReadAllText(filePath);
+                var testCases = (IEnumerable)JsonConvert.DeserializeObject(json);
+
+                foreach(JObject obj in testCases)
+                {
+                    if(template!=null)
+                    {
+                        obj.Merge(template);
+                    }
+                    yield return obj.ToString();
+                }
+            }
+            else
+            {
+                yield return template.ToString();
+            }
+        }
+    }
+}
